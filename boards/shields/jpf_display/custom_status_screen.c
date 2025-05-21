175323156f45cd82928ccd96a70320c9378830c7
janpfischer <mail@jpfischer.de>
2025-05-21 09:56:11 +0200

    adds two new widgets: WPM calculator and layer widget

--- boards/shields/jpf_display/custom_status_screen.c
+++ boards/shields/jpf_display/custom_status_screen.c
@@ -7,6 +7,8 @@
 #include "custom_status_screen.h"
 #include "widgets/output_status.h"
 #include "widgets/battery_status.h"
+#include "widgets/wpm_status.h"
+#include "widgets/layer_status.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -15,6 +17,8 @@ lv_style_t global_style;
 
 static struct zmk_widget_output_status output_status_widget;
 static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
+static struct zmk_widget_wpm_status wpm_status_widget;
+static struct zmk_widget_layer_status layer_status_widget;
 lv_obj_t *zmk_display_status_screen()
 {
     lv_obj_t *screen;
@@ -24,17 +28,23 @@ lv_obj_t *zmk_display_status_screen()
     lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
 
     lv_style_init(&global_style);
-    //lv_style_set_text_font(&global_style, &lv_font_unscii_8); // ToDo: Font is not recognized
+    // lv_style_set_text_font(&global_style, &lv_font_unscii_8); // ToDo: Font is not recognized
     lv_style_set_text_color(&global_style, lv_color_white());
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
-
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 10);
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
+
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_MID, 0, 60);
+
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, 60);
+
     LOG_INF("screen loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
     return screen;
---
+++ boards/shields/jpf_display/widgets/wpm_status.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <lvgl.h>
+#include <zephyr/kernel.h>
+
+struct zmk_widget_wpm_status
+{
+    lv_obj_t *obj;
+    lv_obj_t *wpm_label;
+    sys_snode_t node;
+};
+
+int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_wpm_status_obj(struct zmk_widget_wpm_status *widget);
