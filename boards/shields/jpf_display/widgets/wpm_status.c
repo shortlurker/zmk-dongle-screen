b8904560e4b71825bdef87dea632c5859bbb8cc2
janpfischer <mail@jpfischer.de>
2025-05-21 09:56:11 +0200

    adds two new widgets: WPM calculator and layer widget

---
+++ boards/shields/jpf_display/widgets/wpm_status.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/display.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/wpm_state_changed.h>
+
+#include "wpm_status.h"
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+struct wpm_status_state
+{
+    int wpm;
+};
+
+static struct wpm_status_state get_state(const zmk_event_t *_eh)
+{
+    const struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(_eh);
+
+    return (struct wpm_status_state){
+        .wpm = ev ? ev->state : 0};
+}
+
+static void set_wpm(struct zmk_widget_wpm_status *widget, struct wpm_status_state state)
+{
+
+    char wpm_text[12];
+    snprintf(wpm_text, sizeof(wpm_text), "%i", state.wpm);
+    lv_label_set_text(widget->wpm_label, wpm_text);
+}
+
+static void wpm_status_update_cb(struct wpm_status_state state)
+{
+    struct zmk_widget_wpm_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node)
+    {
+        set_wpm(widget, state);
+    }
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state,
+                            wpm_status_update_cb, get_state)
+ZMK_SUBSCRIPTION(widget_wpm_status, zmk_wpm_state_changed);
+
+// output_status.c
+int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent)
+{
+    widget->obj = lv_obj_create(parent);
+    lv_obj_set_size(widget->obj, 240, 60);
+
+    widget->wpm_label = lv_label_create(widget->obj);
+    lv_obj_align(widget->wpm_label, LV_ALIGN_TOP_RIGHT, -80, 40);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    widget_wpm_status_init();
+    return 0;
+}
+
+lv_obj_t *zmk_widget_wpm_status_obj(struct zmk_widget_wpm_status *widget)
+{
+    return widget->obj;
+}
