519c7d563612e130e5cc15858ada89354a2e6822
janpfischer <mail@jpfischer.de>
2025-07-07 09:11:44 +0200

    Use ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT instead of ZMK_SPLIT_BLE_PERIPHERAL_COUNT in set_battery_symbol

--- boards/shields/dongle_screen/src/widgets/battery_status.c
+++ boards/shields/dongle_screen/src/widgets/battery_status.c
@@ -75,7 +75,7 @@ static void draw_battery(lv_obj_t *canvas, uint8_t level, bool usb_present) {
 }
 
 static void set_battery_symbol(lv_obj_t *widget, struct battery_state state) {
-    if (state.source >= ZMK_SPLIT_BLE_PERIPHERAL_COUNT + SOURCE_OFFSET) {
+    if (state.source >= ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT + SOURCE_OFFSET) {
         return;
     }
     LOG_DBG("source: %d, level: %d, usb: %d", state.source, state.level, state.usb_present);
