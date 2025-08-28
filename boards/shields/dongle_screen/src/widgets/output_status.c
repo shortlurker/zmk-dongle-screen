a29bf30b70820bfbb5a1f820f53cb2344b9da4df
Short_Lurker <33041660+shortlurker@users.noreply.github.com>
2025-08-27 17:17:37 -0700

    ESB only Have dongle compile with config_zmk_ble=n Will hide ble info on display if esb only too

--- boards/shields/dongle_screen/src/widgets/output_status.c
+++ boards/shields/dongle_screen/src/widgets/output_status.c
@@ -11,14 +11,17 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
 #include <zmk/event_manager.h>
-#include <zmk/events/ble_active_profile_changed.h>
 #include <zmk/events/endpoint_changed.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/usb.h>
-#include <zmk/ble.h>
 #include <zmk/endpoints.h>
 
 #include "output_status.h"
+
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+    #include <zmk/events/ble_active_profile_changed.h>
+    #include <zmk/ble.h>
+#endif
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -35,12 +38,20 @@ struct output_status_state
 
 static struct output_status_state get_state(const zmk_event_t *_eh)
 {
-    return (struct output_status_state){
-        .selected_endpoint = zmk_endpoints_selected(),                     // 0 = USB , 1 = BLE
-        .active_profile_index = zmk_ble_active_profile_index(),            // 0-3 BLE profiles
-        .active_profile_connected = zmk_ble_active_profile_is_connected(), // 0 = not connected, 1 = connected
-        .active_profile_bonded = !zmk_ble_active_profile_is_open(),        // 0 =  BLE not bonded, 1 = bonded
-        .usb_is_hid_ready = zmk_usb_is_hid_ready()};                       // 0 = not ready, 1 = ready
+    struct output_status_state st;
+
+    st.selected_endpoint = zmk_endpoints_selected();                         // 0 = USB , 1 = BLE
+    #if IS_ENABLED(CONFIG_ZMK_BLE)
+        st.active_profile_index = zmk_ble_active_profile_index();            // 0-3 BLE profiles
+        st.active_profile_connected = zmk_ble_active_profile_is_connected(); // 0 = not connected, 1 = connected
+        st.active_profile_bonded = !zmk_ble_active_profile_is_open();        // 0 = BLE not bonded, 1 = bonded
+    #else
+        st.active_profile_index     = 0;
+        st.active_profile_connected = false;
+        st.active_profile_bonded    = false;
+    #endif
+    st.usb_is_hid_ready = zmk_usb_is_hid_ready();                           // 0 = not ready, 1 = ready
+    return st;
 }
 
 static void set_status_symbol(struct zmk_widget_output_status *widget, struct output_status_state state)
@@ -70,25 +81,31 @@ static void set_status_symbol(struct zmk_widget_output_status *widget, struct ou
         ble_color = "ffffff";
     }
 
-    switch (state.selected_endpoint.transport)
-    {
-    case ZMK_TRANSPORT_USB:
-        snprintf(transport_text, sizeof(transport_text), "> #%s USB#\n#%s BLE#", usb_color, ble_color);
-        break;
-    case ZMK_TRANSPORT_BLE:
-        snprintf(transport_text, sizeof(transport_text), "#%s USB#\n> #%s BLE#", usb_color, ble_color);
-        break;
-    }
+    #if IS_ENABLED(CONFIG_ZMK_BLE)
+        switch (state.selected_endpoint.transport)
+        {
+        case ZMK_TRANSPORT_USB:
+            snprintf(transport_text, sizeof(transport_text), "> #%s USB#\n#%s BLE#", usb_color, ble_color);
+            break;
+        case ZMK_TRANSPORT_BLE:
+            snprintf(transport_text, sizeof(transport_text), "#%s USB#\n> #%s BLE#", usb_color, ble_color);
+            break;
+        }
+    #else
+        snprintf(transport_text, sizeof(transport_text), "> #%s USB#", usb_color);
+    #endif
 
     lv_label_set_recolor(widget->transport_label, true);
     lv_obj_set_style_text_align(widget->transport_label, LV_TEXT_ALIGN_RIGHT, 0);
     lv_label_set_text(widget->transport_label, transport_text);
 
-    char ble_text[12];
+    #if IS_ENABLED(CONFIG_ZMK_BLE)
+        char ble_text[12];
 
-    snprintf(ble_text, sizeof(ble_text), "%d", state.active_profile_index + 1);
-    // lv_obj_set_style_text_align(widget->ble_label, LV_TEXT_ALIGN_RIGHT, 0);
-    lv_label_set_text(widget->ble_label, ble_text);
+        snprintf(ble_text, sizeof(ble_text), "%d", state.active_profile_index + 1);
+        // lv_obj_set_style_text_align(widget->ble_label, LV_TEXT_ALIGN_RIGHT, 0);
+        lv_label_set_text(widget->ble_label, ble_text);
+    #endif
 }
 
 static void output_status_update_cb(struct output_status_state state)
@@ -103,7 +120,9 @@ static void output_status_update_cb(struct output_status_state state)
 ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state,
                             output_status_update_cb, get_state)
 ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_changed);
-ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+    ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
+#endif
 ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
 
 // output_status.c
@@ -115,8 +134,10 @@ int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_ob
     widget->transport_label = lv_label_create(widget->obj);
     lv_obj_align(widget->transport_label, LV_ALIGN_TOP_RIGHT, -10, 10);
 
-    widget->ble_label = lv_label_create(widget->obj);
-    lv_obj_align(widget->ble_label, LV_ALIGN_TOP_RIGHT, -10, 56);
+    #if IS_ENABLED(CONFIG_ZMK_BLE)
+        widget->ble_label = lv_label_create(widget->obj);
+        lv_obj_align(widget->ble_label, LV_ALIGN_TOP_RIGHT, -10, 56);
+    #endif
 
     sys_slist_append(&widgets, &widget->node);
 
